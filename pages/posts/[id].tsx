ea4ad84eb6f8230282d3b991a87b1d88053453dc
Joseph DeChicchis <josephdechicchis@gmail.com>
2023-09-17 15:42:16 -0400

    Update blog stuff

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,8 +2,10 @@ import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
-import utilStyles from "../../styles/utils.module.css";
 import { GetStaticProps, GetStaticPaths } from "next";
+import ReactMarkdown from "react-markdown";
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import { prism as PrismStyle } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
@@ -28,6 +30,7 @@ export default function Post({
   postData: {
     title: string;
     date: string;
+    content: string;
     contentHtml: string;
   };
 }) {
@@ -37,11 +40,34 @@ export default function Post({
         <title>{postData.title}</title>
       </Head>
       <article>
-        <h1 className="font-bold text-2xl pt-8 pb-4">{postData.title}</h1>
-        <div className="">
+        <h1 className="font-bold text-3xl pt-8 pb-4">{postData.title}</h1>
+        <div className="flex flex-row justify-end text-sm text-gray-500">
           <Date dateString={postData.date} />
         </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <ReactMarkdown
+            className="pt-4 [&_h1]:text-2xl [&_h1]:font-bold [&_h2]:text-xl [&_h2]:font-bold [&_h3]:text-lg [&_h3]:font-bold [&_p]:pt-2 [&_ul]:pl-2 [&_ul]:list-inside [&_ul]:list-disc"
+            components={{
+              code({node, inline, className, children, ...props}) {
+                const match = /language-(\w+)/.exec(className || '')
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    {...props}
+                    children={String(children).replace(/\n$/, '')}
+                    style={PrismStyle}
+                    language={match[1]}
+                    PreTag="div"
+                    className="rounded"
+                  />
+                ) : (
+                  <code {...props} className={className}>
+                    {children}
+                  </code>
+                )
+              }
+            }}
+        >
+          {postData.content}
+        </ReactMarkdown>
       </article>
     </Layout>
   );
